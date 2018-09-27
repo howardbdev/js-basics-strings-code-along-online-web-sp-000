012134d1d069db494670229afe6c7810874bd66b
Howard DeVennish <howarddevennish@gmail.com>
2018-09-27 18:40:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser = "Grace Hopper"
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
-const shortMessage = `Welcome to Flatbook, ${currentUser[0]}!`
+const shortGreeting = `Welcome to Flatbook, ${currentUser[0]}!`
 const excitedWelcomeMessage = welcomeMessage.toUpperCase()
